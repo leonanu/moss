a56944d0ae891e0a896618c7901b9d1c035527e2
Nanu <nanu@inanu.net>
2015-06-30 23:19:04 +0800

    nginx uninstall

--- common/uninstall.sh
+++ common/uninstall.sh
@@ -109,10 +109,82 @@ if grep '^MYSQL$' ${INST_LOG} > /dev/null 2>&1 ; then
 
         userdel -r mysql 2>/dev/null
 
-        sed -i "/^MYSQL\$/d" ${INST_LOG}
-        sed -i "/^XTRABACKUP\$/d" ${INST_LOG}
+        sed -i "/^MYSQL$/d" ${INST_LOG}
+        sed -i "/^XTRABACKUP$/d" ${INST_LOG}
 
         succ_msg "MySQL Server has been removed from your system!"
+        sleep 3
+    fi
+fi
+
+## uninstall Nginx
+if grep '^NGINX$' ${INST_LOG} > /dev/null 2>&1 ; then
+    DEL_NGINX_INPUT=0
+    while [ ${DEL_NGINX_INPUT} -eq 0 ]; do
+        read -p "Do you wish to remove Nginx?(y/N)" DEL_NGINX
+        if [[ "${DEL_NGINX}" = 'y' ]] || [[ "${DEL_NGINX}" = 'Y' ]];then
+            DEL_NGINX=y
+            DEL_NGINX_INPUT=1
+        elif [[ -z "${DEL_NGINX}" ]] || [[ "${DEL_NGINX}" = 'n' ]] || [[ "${DEL_NGINX}" = 'N' ]];then
+            DEL_NGINX=n
+            DEL_NGINX_INPUT=1
+        else
+            warn_msg "You can only input y or n."
+            DEL_NGINX_INPUT=0
+        fi
+    done
+
+    succ_msg "\nStarting Uninstall Nginx...\n"
+    if [ "${DEL_NGINX}" = 'y' ];then
+        BAK_NGINX_DOCROOT_INPUT=0
+        while [ ${BAK_NGINX_DOCROOT_INPUT} -eq 0 ]; do
+            read -p "Do you wish to keep Nginx document root?(Y/n)" BAK_NGINX_DOCROOT
+            if [[ -z "${BAK_NGINX_DOCROOT}" ]] || [[ "${BAK_NGINX_DOCROOT}" = 'y' ]] || [[ "${BAK_NGINX_DOCROOT}" = 'Y' ]];then
+                BAK_NGINX_DOCROOT=y
+                BAK_NGINX_DOCROOT_INPUT=1
+            elif [[ "${BAK_NGINX_DOCROOT}" = 'n' ]] || [[ "${BAK_NGINX_DOCROOT}" = 'N' ]];then
+                BAK_NGINX_DOCROOT=n
+                BAK_NGINX_DOCROOT_INPUT=1
+            else
+                warn_msg "You can only input y or n."
+                BAK_NGINX_DOCROOT_INPUT=0
+            fi
+        done
+
+        warn_msg "Stop Nginx..."
+        /etc/init.d/nginx stop
+        sleep 3
+        if (pstree | grep nginx > /dev/null 2>&1) ; then
+            fail_msg "Nginx Fail to Stop!"
+        else
+            succ_msg "Nginx Stoped!"
+        fi
+
+        rm -f /usr/local/nginx
+        rm -rf ${INST_DIR}/nginx-*
+        rm -rf ${INST_DIR}/tengine-*
+        rm -rf /var/log/nginx
+        rm -f /usr/local/etc/logrotate/nginx
+        sed -i "/.*# Logrotate - Nginx.*/d" /var/spool/cron/root
+        sed -i "/.*\/usr\/local\/etc\/logrotate\/nginx.*/d" /var/spool/cron/root
+        if [ "${BAK_NGINX_DOCROOT}" = 'n' ];then
+            rm -rf ${NGX_DOCROOT}
+        fi
+        rm -rf ${NGX_LOGDIR}
+
+        chkconfig --del nginx
+        rm -f /etc/init.d/nginx
+
+        if (pstree | grep php-fpm > /dev/null 2>&1) ; then
+            warn_msg "User www is used by PHP-FPM now."
+            warn_msg "Moss will not delete www user."
+        else
+            userdel -r www 2>/dev/null
+        fi
+
+        sed -i "/^NGINX$/d" ${INST_LOG}
+
+        succ_msg "Nginx has been removed from your system!"
         sleep 3
     fi
 fi
