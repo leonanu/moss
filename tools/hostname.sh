9329c6f5c01a607189e43367f38faa6db56073c7
Nanu <nanu@inanu.net>
2015-12-06 21:37:24 +0800

    update hostname.sh

--- tools/hostname.sh
+++ tools/hostname.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-read -p 'Input hostname: ' OS_HOSTNAME
+OLD_HOSTNAME="$(hostname)"
+NEW_HOSTNAME="$1"
 
-hostname $OS_HOSTNAME
-sed -i "s#HOSTNAME.*#HOSTNAME=${OS_HOSTNAME}#" /etc/sysconfig/network
-sed -i "s#127.*#127.0.0.1  localhost ${OS_HOSTNAME}#" /etc/hosts
+if [ -z "$NEW_HOSTNAME" ]; then
+    echo -n "Please enter new hostname: "
+    read NEW_HOSTNAME < /dev/tty
+fi
 
-echo 'Hostname has been set, you need logout and login agin to take effect.'
+if [ -z "$NEW_HOSTNAME" ]; then
+    echo "Error: No hostname entered. Exiting."
+    exit 1
+fi
+
+echo "Changing hostname from $OLD_HOSTNAME to $NEW_HOSTNAME..."
+
+hostname "$NEW_HOSTNAME"
+
+sed -i "s/HOSTNAME=.*/HOSTNAME=$NEW_HOSTNAME/g" /etc/sysconfig/network
+
+if [ -n "$( grep "$OLD_HOSTNAME" /etc/hosts )" ]; then
+    sed -i "s/$OLD_HOSTNAME//g" /etc/hosts
+fi
+
+sed -i "s/127\.0\.0\.1.*/127.0.0.1    $NEW_HOSTNAME localhost localhost.localdomain/" /etc/hosts
